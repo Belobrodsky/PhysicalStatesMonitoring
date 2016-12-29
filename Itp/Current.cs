93dbd14a860f868e04882c34ecd824c44d2a22e1
MMShatalin <mmshatalin@mail.ru>
2016-12-29 10:29:30 +0300

    Изменения в классе Current

--- Itp/Current.cs
+++ Itp/Current.cs
@@ -7,11 +7,11 @@ namespace Ipt
         class MyConst
         {
             //параметры запаздывающих нейтронов (постоянные распада - лямбда, взятые из методик физических испытаний)
-            public static double[] l_metodiki = { 0.0127, 0.0317, 0.1180, 0.3170, 1.4000, 3.9200 };
+            public static double[] LMetodiki = { 0.0127, 0.0317, 0.1180, 0.3170, 1.4000, 3.9200 };
             //параметры запаздывающих нейтронов (относительные групповые доли - альфа)
-            public static double[] aAPIK = { 0.0340, 0.2020, 0.1840, 0.4030, 0.1430, 0.0340 };
+            public static double[] AApik = { 0.0340, 0.2020, 0.1840, 0.4030, 0.1430, 0.0340 };
             //коэффициент перевода из процентов в бетта эффективность
-            public static double Beff = 0.74;
+            private static double _beff = 0.74;
         }
 
         public double Tok1 { get; set; }
@@ -22,59 +22,66 @@ namespace Ipt
 
         public void SearchCurrent(Ipt4 temp)
         {
-            double step1 = Math.Pow(10, -temp.Power1);
-            double step2 = Math.Pow(10, -temp.Power2);
-            Tok1 = (temp.FCurrent1 / 25000.0) * step1;
-            Tok2 = (temp.FCurrent2 / 25000.0) * step2;
+            var step1 = Math.Pow(10, -temp.Power1);
+            var step2 = Math.Pow(10, -temp.Power2);
+            this.Tok1 = (temp.FCurrent1 / 25000.0) * step1;
+            this.Tok2 = (temp.FCurrent2 / 25000.0) * step2;
         }
         //для расчета реактивностей необходимы следующие параметры
         //предыдущее значение времени
-        private double Time_Old;
+        private double _timeOld;
         //последнее значение времени зарегистрированного параметра
-        private double Time_Now;
+        private double _timeNow;
         public double Ro; ////возвращает реактивность, рассчитанную в Беттах
         //разность времени последнего и предпоследнего значений времени в формате UNIX
         public double dT;
 
         //6 групп запаздывающих нейтронов
         //два массива для упрощения вычислений реактивности в дальнейшем
-        double[] one = new double[6];
-        double[] two = new double[6];
+        double[] _one = new double[6];
+        double[] _two = new double[6];
 
         //для поиска реактивности по формуле обращенного уравнения кинетики
         //это массив первых 6-ти значений, они все равны первому вычисленному току
         //так как нет предыдущего времени, а есть только первое значение
-        double[] psi0 = new double[6];
+        double[] _psi01 = new double[6];
+        double[] _psi02 = new double[6];
         //  List<double> MyPsi = new List<double>();
 
 
         //эти методы должны расчитывать реактивности из Ток1 и Ток2
-        //так как предыдущего значения тока нет , то приравиваем все 6 значений массива одному току
-        public void PSI0(double J_Old)
+        public void SearchReactivity(double timeNow, double timeOld, double j1Now, double j1Old, double j2Now, double j2Old, double[] l, double[] a)
         {
+            //так как предыдущего значения тока нет в самом начале регистрации, то приравиваем все 6 значений массива одному току
+            //TODO:ВОЗМОЖНО ТУТ НУЖНО СТАВИТЬ УСЛОВИЕ, ЕСЛИ НАЧАЛО РЕГИСТРАЦИИ ТО НУЖНО ВЫПОЛНЯТЬ ЭТОТ ЦИКЛ 
+            //TODO:ТАК КАК НЕТ ДВУХ ЗНАЧЕНИЙ ТОКОВ, А ЕСТЬ ТОЛЬКО ОДНО, А ЕСЛИ ЗАРЕГИСТРИРОВАЛОСЬ ВТОРОЕ ЗНАЧЕНИЕ 
+            //TODO:ПРОПУСКАЕТСЯ ЭТОТ ЦИКЛ FOR
             for (int i = 0; i < 6; i++)
-            {
-                psi0[i] = J_Old;
-            }
-        }
-        //метод когда уже более 1-го значения тока для расчета реактивности на каждом шаге
-        public double SearchReactivity(double Time_Now, double Time_Old, double J_Now, double J_Old, double[] l, double[] a)
-        {
-            double Reactivity = 0;
+                {
+                    _psi01[i] = j1Old;
+                    _psi02[i] = j2Old;
+                }
+            
+        
+            this.Reactivity1 = 0;
+            this.Reactivity2 = 0;
 
-            dT = Time_Now - Time_Old;
+            dT = timeNow - timeOld;
 
-            for (int i = 0; i < one.Length; i++)
+            for (int i = 0; i < _one.Length; i++)
             {
-                double Const_t_raspada = l[i] * dT;
-                one[i] = Math.Exp(-Const_t_raspada);
-                two[i] = (1 - one[i]) / Const_t_raspada;
-                psi0[i] = psi0[i] * one[i] - (J_Now - J_Old) * (two[i]) - J_Old * one[i] + J_Now;
-                double yt = a[i] * psi0[i];
-                Reactivity = Reactivity + yt;
+                double constTRaspada = l[i] * dT;
+                _one[i] = Math.Exp(-constTRaspada);
+                _two[i] = (1 - _one[i]) / constTRaspada;
+                _psi01[i] = _psi01[i] * _one[i] - (j1Now - j1Old) * (_two[i]) - j1Old * _one[i] + j1Now;
+              //изменить название дикое
+                var item1 = a[i] * _psi01[i];
+                var item2 = a[i] * _psi02[i];
+                this.Reactivity1 = this.Reactivity1 + item1;
+                this.Reactivity2 = this.Reactivity2 + item2;
             }
-            Reactivity = 1 - Reactivity / J_Now;
-            return Reactivity;
+            this.Reactivity1 = 1 - this.Reactivity1/j1Now;
+            this.Reactivity2 = 1 - this.Reactivity2/j2Now;
         }
     }
 }
