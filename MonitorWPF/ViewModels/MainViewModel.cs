f0ca8bbdf9ef4ddee1d28d282e1123c5d27a432f
viter.alex <viter.alex@gmail.com>
2017-01-15 17:55:43 +0200

    Добавлен проект MonitorWPF

---
+++ MonitorWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+using MonitorWPF.Commands;
+
+namespace MonitorWPF.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private DelegateCommand exitCommand;
+
+        #region Constructor
+
+        public MainViewModel()
+        {
+            // Blank
+        }
+
+        #endregion
+
+        public ICommand ExitCommand
+        {
+            get
+            {
+                if (exitCommand == null)
+                {
+                    exitCommand = new DelegateCommand(Exit);
+                }
+                return exitCommand;
+            }
+        }
+
+        private void Exit()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
